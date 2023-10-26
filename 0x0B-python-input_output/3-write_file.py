f774a36c0b8a4ed3e362035cc43be47a8f5f50dc
alphonsi <123053150+alphonsi@users.noreply.github.com>
2023-10-26 14:02:18 +0300

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/3-write_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" contains the write_file function """
-
-
-def write_file(filename="", text=""):
-    """ write a string to a text file and return num of char written"""
-    with open(filename, 'w', encoding='utf-8') as f:
-        return f.write(text)
--- 0x0B-python-input_output/5-to_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" contains the to_json_string function """
-import json
-
-
-def to_json_string(my_obj):
-    """ returns JSON representation of an object """
-    return json.dumps(my_obj)
